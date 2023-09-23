587c805af73b3d6bd2a9bbb9a4ad8bb16d789982
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-09-23 16:28:32 -0300

    Adicionei o protocolo CORS do flask e adicionei o método POST na rota de simplifica

--- API/main.py
+++ API/main.py
@@ -1,9 +1,16 @@
 # código principal da API de simplificação de expressões booleanas
 from flask import Flask, redirect, url_for, render_template, request
+from flask_cors import CORS
 from sympy import sympify, simplify_logic
 
 
 app = Flask(__name__)
+CORS(app)   # permite que qualquer despositivo acesse a API
+
+# -> caso queira definir o local que poderá acessar a API
+# CORS(app, origins=['http://localhost:3000'])  # Permitindo solicitações apenas de http://localhost:3000
+
+
 
 # ROTA BASE DO SISTEMA DO SERVIDOR
 @app.route('/')
@@ -11,14 +18,22 @@ def welcome():
 	return "BEM VINDO AO SISTEMA DE SIMPLIFICAÇÃO DE ALGEBRA BOOLEANA"
 
 
-# ROTA QUE REALIZA A SIMPLIFICAÇAÕ DA EXPRESSÃO BOOLEANA
-@app.route('/simplifica/<string:expressao>')    # pega a string que foi enviada e simplifica
-def simplifica(expressao):
-    exp_sympy = sympify(expressao)
+# ROTA QUE REALIZA A SIMPLIFICAÇAÕ DA EXPRESSÃO BOOLEANA -> teste na própria API
+# @app.route('/simplifica/<string:expressao>')    # pega a string que foi enviada e simplifica
+# def simplifica(expressao):
+#     exp_sympy = sympify(expressao)
+#     exp_simpli = simplify_logic(exp_sympy)
+#     return str(exp_simpli)
+
+# ROTA QUE REALIZA A SIMPLIFICAÇÃO DA EXPRESSÃO BOOLEANA -> usando uma aplicação externa
+@app.route('/simplifica', methods=['POST'])
+def simplifica():
+    data = request.form.get('data')  # Obtém o valor da chave 'data' do corpo da solicitação POST
+    exp_sympy = sympify(data)
     exp_simpli = simplify_logic(exp_sympy)
     return str(exp_simpli)
 
 
 if __name__=='__main__':
-	app.run(debug=True)     # ATIVA A DEBUGAÇÃO -> mudanças no código acontecem em tempo real na API
-	# app.run()
+	# app.run(debug=True)     # ATIVA A DEBUGAÇÃO -> mudanças no código acontecem em tempo real na API
+    app.run()
