ef7ea48a777d885801b56852c1a763aa707af4d8
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-09-23 16:30:05 -0300

    Adicionei uma tela onde podemos fazer a requisição para a API e pegamos o retorno(simplificação da expressão booleana), roda localmente

---
+++ app_python/lib/MyApiScream.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class MyAPIScreen extends StatefulWidget {
+  @override
+  _MyAPIScreenState createState() => _MyAPIScreenState();
+}
+
+class _MyAPIScreenState extends State<MyAPIScreen> {
+  String _inputText = '';
+  String _responseText = '';
+
+  Future<void> _sendDataToAPI() async {
+    final url = "http://127.0.0.1:5000/simplifica";
+
+    final response =
+        await http.post(Uri.parse(url), body: {"data": _inputText});
+
+    if (response.statusCode == 200) {
+      // A solicitação foi bem-sucedida
+      final responseData = response.body;
+      setState(() {
+        _responseText = responseData;
+      });
+    } else {
+      // A solicitação falhou
+      print('Erro na solicitação: ${response.statusCode}');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Exemplo de Integração de API'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            TextField(
+              onChanged: (value) {
+                setState(() {
+                  _inputText = value;
+                });
+              },
+              decoration: const InputDecoration(
+                labelText: 'Digite uma string',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: _sendDataToAPI,
+              child: const Text('Enviar para API'),
+            ),
+            const SizedBox(height: 20.0),
+            const Text(
+              'Resposta da API:',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text(_responseText),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- app_python/lib/main.dart
+++ app_python/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:app_python/MyApiScream.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,43 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  Future<void> sendDataToAPI() async {
-    final apiUrl = 'http://10.0.44.30:5000/';
-    final data = {'key': 'value'}; // Dados que você quer enviar para a API
-
-    final response = await http.post(
-      Uri.parse(apiUrl),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode(data),
-    );
-
-    if (response.statusCode == 200) {
-      print('API Response: ${response.body}');
-    } else {
-      print('Failed to send data to API');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter API Integration'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            sendDataToAPI(); // Chame a função aqui quando o botão for pressionado
-          },
-          child: Text('Enviar Dados para a API'),
-        ),
-      ),
+      title: 'My App',
+      home: MyAPIScreen(),
     );
   }
 }
