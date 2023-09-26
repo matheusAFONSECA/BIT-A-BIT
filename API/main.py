016d81255b1c8727930d59831aff0deb50b07d5c
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-09-26 14:31:43 -0300

    função de simplificação de expressão por meio da tabela da verdade

--- API/main.py
+++ API/main.py
@@ -1,7 +1,9 @@
 # código principal da API de simplificação de expressões booleanas
 from flask import Flask, redirect, url_for, render_template, request
 from flask_cors import CORS
-from sympy import sympify, simplify_logic
+from sympy import sympify, simplify_logic, symbols
+from sympy.logic.boolalg import truth_table, SOPform
+import re
 
 
 app = Flask(__name__)
@@ -35,7 +37,36 @@ def simplifica():
     return str(exp_simpli)
 
 
+#Simplificar expressão por meio da tabela verdade
+@app.route('/simptabela/<string:mapa>')
+def simpTabela(mapa):
+  #Definir minitermos e calcular num de variáveis
+  #Remover caracteres de separação do mapa
+  valores = re.sub("{|}|(\s*\d+:\s*)","", mapa)
+  minitermos = []
+  contador = 0
+  for valores in re.split(",", valores):
+    if(valores=='1'):
+      minitermos.append(contador)
+    contador += 1
+  #Quantidade de variáveis
+  if(contador==4):
+    A, B = symbols('A B')
+    variaveis = [A, B]
+  elif(contador==8):
+    A, B, C = symbols('A B C')
+    variaveis = [A, B, C]
+  elif(contador==16):
+    A, B, C, D = symbols('A B C D')
+    variaveis = [A, B, C, D]
+  elif(contador==32):
+    A, B, C, D, E  = symbols('A B C D E')
+    variaveis = [A, B, C, D, E]
+  exp = SOPform(variaveis, minitermos)
+  return str(exp)
+
+
 if __name__=='__main__':
-	# app.run(debug=True)     # ATIVA A DEBUGAÇÃO -> mudanças no código acontecem em tempo real na API
+	app.run(debug=True)     # ATIVA A DEBUGAÇÃO -> mudanças no código acontecem em tempo real na API
     # app.run()               # Roda a API na porta padrão http://127.0.0.1:5000/ -> web, no PC mesmo
-    app.run(host='0.0.0.0', port=8080)    # roda a API em uma porta e host que específicamos -> emulador
+    # app.run(host='0.0.0.0', port=8080)    # roda a API em uma porta e host que específicamos -> emulador
